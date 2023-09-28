e3acafbe99a9577eef6c18b63e3a83ec156147f3
mtk3281 <tkmunthasar@gmail.com>
2023-09-28 14:27:04 +0530

    Updated steve

--- Heavenly Knight 2.0/steve.gd
+++ Heavenly Knight 2.0/steve.gd
@@ -9,7 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction
 
 func _physics_process(delta):
-	print("hi")
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
